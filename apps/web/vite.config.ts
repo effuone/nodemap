6055fb504057655e55e6ac29972f38e9d2af608d
effuone <65224356+effuone@users.noreply.github.com>
2024-03-30 12:11:19 +0500

    @shadcn/ui added

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    port: 3000,
+    strictPort: true,
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+    origin: 'http://0.0.0.0:3000',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
