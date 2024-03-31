23dbdfc491947afa1f92f7bb6d1adef66ad6aaab
Sanzhar <sanxzhar@gmail.com>
2024-03-31 12:42:46 +0500

    childPathEdge

---
+++ apps/web/src/components/childPathEdge.tsx
@@ -0,0 +1,43 @@
+import { FC } from 'react';
+import { BaseEdge, getStraightPath } from 'reactflow';
+
+interface ChildPathEdgeProps {
+  id: string;
+  sourceX: number;
+  sourceY: number;
+  targetX: number;
+  targetY: number;
+}
+
+const ChildPathEdge: FC<ChildPathEdgeProps> = ({
+  id,
+  sourceX,
+  sourceY,
+  targetX,
+  targetY,
+}) => {
+  const [edgePath] = getStraightPath({
+    sourceX,
+    sourceY,
+    targetX,
+    targetY,
+  });
+
+  return (
+    <>
+      <BaseEdge
+        id={id}
+        path={edgePath}        
+        style={{
+          stroke: '#616161', // Edge color
+          strokeWidth: 1, // Edge thickness
+          strokeLinecap: 'round', // Edge line cap style
+          strokeDasharray: '0', // Solid line; use a value like '5,5' for dashed lines
+          fill: 'none', // Ensures the path is not filled
+        }}
+      />
+    </>
+  );
+};
+
+export default ChildPathEdge;
