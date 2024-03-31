f6690fc87bcc380bd5243866e928f7382de57a9f
effuone <65224356+effuone@users.noreply.github.com>
2024-03-31 17:46:43 +0500

    the feature of the 21ct century implemented

--- apps/web/src/components/mainNode.tsx
+++ apps/web/src/components/mainNode.tsx
@@ -11,16 +11,18 @@ interface MainNodeProps extends NodeProps {
 const MainNode: FC<MainNodeProps> = ({
   data,
   sourcePosition = Position.Top,
-  targetPosition = Position.Right,  
-  childNodePosition = Position.Right,
+  targetPosition = Position.Right,
 }) => {
   return (
-    <div className='min-w-[150px] min-h-[50px] px-4 py-4 flex justify-center items-center rounded-[8px] bg-white'>
-      <div className='text-center text-[17px] font-semibold text-black'>{data.label}</div>
-      <Handle type='source' position={sourcePosition} className='' id='1' /> {/* откуда он отдает */}
-      <Handle type='target' position={targetPosition} className='' /> {/* откуда ему приходить */}
-
-      <Handle type='source' position={childNodePosition} className='' id='2' /> {/* откуда он с child connect */}
+    <div className="min-w-[150px] min-h-[50px] px-4 py-4 flex justify-center items-center rounded-[8px] bg-white">
+      <div className="text-center text-[17px] font-semibold text-black">
+        {data.label}
+      </div>
+      <Handle type="source" position={sourcePosition} className="" id="1" />{' '}
+      {/* откуда он отдает */}
+      <Handle type="target" position={targetPosition} className="" />{' '}
+      {/* откуда ему приходить */}
+      {/* <Handle type='source' position={childNodePosition} className='' id='2' /> откуда он с child connect */}
     </div>
   );
 };
--- apps/web/src/pages/RoadmapPage.tsx
+++ apps/web/src/pages/RoadmapPage.tsx
@@ -1,142 +1,193 @@
-import MainNode from "@/components/mainNode";
-import MainPathEdge from "@/components/mainPathEdge";
-import { Button } from "@/components/ui/button";
-import Layout from "@/layout/layout";
-import { applyCommonProperties, constructMainPathEdges } from "@/lib/utils";
-import { useCallback, useEffect, useState } from "react";
+import MainNode from '@/components/mainNode';
+import MainPathEdge from '@/components/mainPathEdge';
+import { Button } from '@/components/ui/button';
+import Layout from '@/layout/layout';
+import { useCallback, useEffect, useState } from 'react';
 import ReactFlow, {
   Background,
   BackgroundVariant,
   Controls,
+  Edge,
   Node,
   Position,
-  addEdge,
   useEdgesState,
   useNodesState,
-} from "reactflow";
-import "reactflow/dist/style.css";
-import { io } from "socket.io-client";
-import { applyEdgeChanges } from "reactflow";
-import DialogCustom from "@/components/dialog-custom";
-import ChildNode from "@/components/childNode";
-import ChildPathEdge from "@/components/childPathEdge";
+} from 'reactflow';
+import 'reactflow/dist/style.css';
+import { io } from 'socket.io-client';
+import { applyEdgeChanges } from 'reactflow';
+import ChildNode from '@/components/childNode';
+import ChildPathEdge from '@/components/childPathEdge';
 
 type GPTNode = {
-  title?: string;
-  details?: string[];
+  title: string;
+  details: string[];
 };
 
-const nodeTypes = { mainNode: MainNode, childNode: ChildNode };
-const edgeTypes = { mainPathEdge: MainPathEdge, childPathEdge: ChildPathEdge };
-
-const exampleDialogProps: any = {
-  title: "HTML",
-  info: "HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser. It defines the content and structure of web content.",
-  links: [
-    {
-      title: "W3Schools: Introduction to HTML",
-      link: "https://www.w3schools.com/",
-    },
-    {
-      title: "MDN Web Docs: HTML",
-      link: "https://developer.mozilla.org/en-US/docs/Web/HTML",
-    },
-    {
-      title: "GeeksforGeeks: HTML Tutorial",
-      link: "https://www.geeksforgeeks.org/html-tutorials/",
-    },
-  ],
+const nodeTypes = {
+  mainNode: MainNode,
+  childNode: ChildNode,
 };
+const edgeTypes = {
+  mainPathEdge: MainPathEdge,
+  childPathEdge: ChildPathEdge,
+};
+
+let yPosMainNode = 0;
+const yGapMainNode = 150;
+const yPosDetailNodeStart = 50;
+const yGapDetailNode = 100;
+let leftSum = 0;
+let rightSum = -200;
 
 const RoadmapPage = () => {
-  // MODAL DIALOG CODE STARTS HERE
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleOpen = () => setIsModalOpen(true);
-  const handleClose = () => setIsModalOpen(false);
-
-  // MODAL DIALOG EXAMPLE CODE ENDS HERE
-
-  const [nodes, setNodes, onNodesChange] = useNodesState(
-    applyCommonProperties([
-      {
-        position: { x: 0, y: 0 },
-        sourcePosition: Position.Bottom,
-        targetPosition: Position.Top,
-        data: { label: "Internet" },
-      },
-    ]),
-  );
 
   const [newNode, setNewNode] = useState<GPTNode>();
 
-  const [edges, setEdges] = useEdgesState(constructMainPathEdges(nodes));
+  const [nodes, setNodes, onNodesChange] = useNodesState([]);
+
+  const [edges, setEdges] = useEdgesState([]);
+
+  const addNode = (newNode: Node) => {
+    setNodes((oldNodes) => [...oldNodes, newNode]);
+  };
+
+  const addEdge = (newEdge: Edge) => {
+    setEdges((oldEdges) => [...oldEdges, newEdge]);
+  };
+
+  const getMainNodes = () => {
+    return nodes.filter((node) => node.type === 'mainNode');
+  };
+
+  useEffect(() => {
+    if (!newNode) {
+      return;
+    }
+    const mainNodes = getMainNodes();
+    const nodeId = mainNodes.length !== 0 ? mainNodes.length + 1 : 1;
+    console.log('nodeId', nodeId);
+    const mainNodeId = `node_${nodeId}`;
+    yPosMainNode += yGapMainNode;
+
+    let yPosDetailNode = yPosDetailNodeStart;
+
+    const groupNodeIndex = `node_${nodeId}_children`;
+
+    //add group node for upcoming children
+    const groupSize = newNode.details.length * yGapDetailNode;
+
+    if (nodeId % 2 !== 0) {
+      leftSum += groupSize + 50; // Increment the sum by the group size plus a gap
+    } else {
+      rightSum += groupSize + 50; // Increment the sum by the group size plus a gap
+    }
+
+    addNode({
+      id: mainNodeId,
+      position: { x: 0, y: yPosMainNode },
+      type: 'mainNode',
+      sourcePosition: Position.Bottom,
+      targetPosition: Position.Top,
+      data: { label: newNode.title },
+    });
+
+    addNode({
+      id: groupNodeIndex,
+      type: 'group',
+      position: {
+        x: nodeId % 2 != 0 ? -250 : 250,
+        y: nodeId % 2 != 0 ? rightSum : leftSum,
+      },
+      style: {
+        width: 150,
+        height: groupSize,
+        border: 'none',
+      },
+      data: { label: '' },
+    });
+
+    let incrementalStep = 0;
+
+    // Create child nodes for each detail and connect them to the main node
+    newNode.details.forEach((detail, index) => {
+      const childId = index + 1;
+      const detailNodeId = `node_${nodeId}_child_${childId}`;
+      addNode({
+        id: detailNodeId,
+        type: 'childNode',
+        position: {
+          x: 0,
+          y: incrementalStep,
+        },
+        data: { label: detail },
+        sourcePosition: nodeId % 2 !== 0 ? Position.Left : Position.Right,
+        targetPosition: Position.Bottom,
+        //@ts-ignore
+        parent: 'extent',
+        parentNode: groupNodeIndex,
+      });
+      incrementalStep = incrementalStep + 100;
+
+      // Connect the main node to this detail node
+      addEdge({
+        id: `node_${nodeId}_to_child_${childId}`,
+        source: mainNodeId,
+        target: detailNodeId,
+        type: 'childPathEdge',
+      });
+
+      // Update position for next detail node
+      yPosDetailNode += yGapDetailNode;
+    });
+
+    if (nodeId > 1) {
+      // If there's a previous main node, create an edge to connect them
+      addEdge({
+        id: `edge_node_${nodeId - 1}_to_node_${nodeId}`,
+        source: `node_${nodeId - 1}`,
+        target: mainNodeId,
+        type: 'mainPathEdge', // Make sure this type matches your defined edge types
+      });
+    }
+  }, [newNode]);
 
   const onEdgesChange = useCallback(
     (changes: any) => {
       setEdges((oldEdges) => applyEdgeChanges(changes, oldEdges));
     },
-    [setEdges],
+    [setEdges]
   );
 
-  const userPrompt = "Create a roadmap for becoming full-stack engineer";
-
-  useEffect(() => {
-    if (!newNode) return;
-
-    setNodes((currentNodes: any) => {
-      const lastNode = currentNodes[currentNodes.length - 1];
-      const lastNodeYPosition = lastNode ? lastNode.position.y : 0;
-      const lastNodeIndex = lastNode ? (Number(lastNode.id) as number) + 1 : 1;
-
-      const newNodeObject: Node = {
-        id: `${lastNodeIndex}`,
-        type: "mainNode",
-        position: { x: lastNode.position.x, y: lastNodeYPosition + 140 },
-        data: {
-          label: newNode.title,
-        },
-        sourcePosition: lastNode.sourcePosition,
-        targetPosition: lastNode.targetPosition,
-      };
-
-      return [...currentNodes, newNodeObject];
-    });
-  }, [newNode]);
-
-  useEffect(() => {
-    setEdges(constructMainPathEdges(nodes));
-  }, [nodes]);
+  const userPrompt = 'Create a roadmap for becoming full-stack engineer';
 
   const startStreaming = () => {
-    const socket = io("http://localhost:7070");
+    const socket = io('http://localhost:7070');
 
-    socket.on("connect", () => {
-      console.log("Socket connected");
-      socket.emit("createRoadmap", userPrompt);
+    socket.on('connect', () => {
+      console.log('Socket connected');
+      socket.emit('createRoadmap', userPrompt);
     });
 
-    socket.on("streamData", (data: GPTNode) => {
+    socket.on('streamData', (data: GPTNode) => {
       setNewNode(data);
     });
 
-    socket.on("disconnect", () => {
-      console.log("Socket disconnected");
+    socket.on('disconnect', () => {
+      console.log('Socket disconnected');
     });
 
-    socket.on("connect_error", (error) => {
-      console.error("Connection error:", error);
+    socket.on('connect_error', (error) => {
+      console.error('Connection error:', error);
     });
   };
 
-  const onConnect = useCallback(
-    (connection: any) => setEdges((eds) => addEdge(connection, eds)),
-    [setEdges],
-  );
-
   return (
     <>
-      <div style={{ width: "100vw", height: "100vh" }}>
+      <div style={{ width: '100vw', height: '100vh' }}>
         <Layout>
           <Button
             onClick={startStreaming}
@@ -158,19 +209,18 @@ const RoadmapPage = () => {
             edges={edges}
             onNodesChange={onNodesChange}
             onEdgesChange={onEdgesChange}
-            onConnect={onConnect}
           >
-            <Background style={{backgroundColor: "#282828"}} color="#3E3E3E" variant={'dots' as BackgroundVariant} gap={12} size={2} /> 
+            <Background
+              style={{ backgroundColor: '#282828' }}
+              color="#3E3E3E"
+              variant={'dots' as BackgroundVariant}
+              gap={12}
+              size={2}
+            />
             <Controls />
           </ReactFlow>
         </Layout>
       </div>
-      <DialogCustom
-        info={exampleDialogProps.info}
-        title={exampleDialogProps.title}
-        isOpen={isModalOpen}
-        onClose={handleClose}
-      />
     </>
   );
 };
