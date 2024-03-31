102ce44416e799544e3229afccfa7fea19bc7c1a
effuone <albkfil@gmail.com>
2024-03-31 19:31:40 +0500

    node connections fixed

--- apps/web/src/components/childNode.tsx
+++ apps/web/src/components/childNode.tsx
@@ -15,8 +15,8 @@ const ChildNode: FC<childNodeProps> = ({
   return (
     <div className=" min-w-[150px] min-h-[50px] px-4 py-4 flex justify-center items-center rounded-[8px] border-[1px] border-[#474747] bg-[#282828] ">
       <div className="text-center text-white text-[17px]">{data.label}</div>
-      <Handle type="target" position={targetPosition} className="bg-black " />
-      <Handle type="source" position={sourcePosition} className="bg-black " />
+      <Handle type="source" position={targetPosition} className="bg-black " />
+      <Handle type="target" position={sourcePosition} className="bg-black " />
     </div>
   );
 };
