4a3720d300b08899db7fc8377bb81b6c86123f4f
zhqriks <zhorikmartanov228@gmail.com>
2024-03-31 14:12:49 +0500

    add landing

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { ThemeProvider } from './components/theme-provider.tsx';
-import { RouterProvider } from 'react-router-dom';
-import { Toaster } from './components/ui/toaster.tsx';
-import router from './routes.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { ThemeProvider } from "./components/theme-provider.tsx";
+import { RouterProvider } from "react-router-dom";
+import { Toaster } from "./components/ui/toaster.tsx";
+import router from "./routes.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <RouterProvider router={router} />
       <Toaster />
     </ThemeProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
--- apps/web/src/routes.tsx
+++ apps/web/src/routes.tsx
@@ -1,13 +1,14 @@
-import { AuthProvidingLayout } from './lib/auth/auth.layout';
-import { useAuth } from './lib/hooks/useAuth';
+import { AuthProvidingLayout } from "./lib/auth/auth.layout";
+import { useAuth } from "./lib/hooks/useAuth";
 import {
   Navigate,
   Route,
   createBrowserRouter,
   createRoutesFromElements,
-} from 'react-router-dom';
-import AuthPage from './pages/AuthPage';
-import RoadmapPage from './pages/RoadmapPage';
+} from "react-router-dom";
+import AuthPage from "./pages/AuthPage";
+import RoadmapPage from "./pages/RoadmapPage";
+import HomePage from "./pages/HomePage";
 
 const DefaultRoute = () => {
   const user = useAuth()?.user;
@@ -22,11 +23,11 @@ const DefaultRoute = () => {
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<AuthProvidingLayout />}>
-      <Route path="/" element={<DefaultRoute />} />
-      <Route path={'/auth'} element={<AuthPage />} />
-      <Route path={'/roadmap'} element={<RoadmapPage />} />
-    </Route>
-  )
+      <Route path="/" element={<HomePage />} />
+      <Route path={"/auth"} element={<AuthPage />} />
+      <Route path={"/roadmap"} element={<RoadmapPage />} />
+    </Route>,
+  ),
 );
 
 export default router;
